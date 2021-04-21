90505380f053604400a2f12cf2cdd675b64a87f2
sfikouris <parissf15@hotmail.com>
2021-04-21 15:55:21 +0300

    asd

--- ICT4D_prototype/views.py
+++ ICT4D_prototype/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 from ICT4D_prototype.models import Person
@@ -17,11 +18,9 @@ def add(request):
 def data(request):
         name = request.GET['name']
         return HttpResponse(name)
-
+        
+@csrf_exempt
 def dummy(request):
-    num1 = request.POST['where']
-    record = request.POST['testvoice']
-    name = Person(first_name=num1, last_name="test", voice = record )
     #num1 = request.GET['where']
     record = request.GET['voice']
     name = Person(first_name="one", last_name="test", voice = record )
