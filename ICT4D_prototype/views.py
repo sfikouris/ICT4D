cf41e1059d5ffddd3b75113553beb983247d6c39
stijnschout3n <stijnscho@gmail.com>
2021-04-21 12:05:14 +0200

    changes

--- ICT4D_prototype/views.py
+++ ICT4D_prototype/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from ICT4D_prototype.models import Person
+
+
 def home(request):
     return render(request, 'home.html', {'name' : 'ICT4'})
 
@@ -12,6 +15,10 @@ def add(request):
     return render(request, 'result.html', {'result':res})
 
 def data(request):
-        
         name = request.GET['name']
         return HttpResponse(name)
+
+def dummy(request):
+    num1 = str(request.GET['where'])
+    name = Person(first_name=num1, last_name="test")
+    name.save()
