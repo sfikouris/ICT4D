7e0b1f213d74e7d5de13f164d04e41579515c1bf
sfikouris <parissf15@hotmail.com>
2021-05-09 10:56:52 +0300

    asd

--- ICT4D_prototype/form.py
+++ ICT4D_prototype/form.py
@@ -4,5 +4,5 @@ from .models import Document
 class DocumentForm(forms.ModelForm):
     class Meta:
         model = Document
-        fields = ('rec_commune','rec_location','rec_name','tree_num',)
+        fields = ('rec_commune','rec_location','rec_name','tree_num','phone',)
         #'cercle_num','tree_count','chosen_language','phone',
--- ICT4D_prototype/models.py
+++ ICT4D_prototype/models.py
@@ -12,12 +12,12 @@ class treeaid(models.Model):
     phone_number = models.CharField(max_length=30)
 
 class Document(models.Model):
-    rec_commune = models.FileField(upload_to='media/')
-    rec_location  = models.FileField(upload_to='media/')
+    rec_commune = models.FileField(upload_to='media/',null=True)
+    rec_location  = models.FileField(upload_to='media/',null=True)
     rec_name = models.FileField(upload_to='media/',null=True)
     #cercle_num = models.CharField(max_length=30, null=True)
     tree_num  = models.CharField(max_length=30, null=True)
     #tree_count = models.IntegerField()
     #chosen_language = models.CharField(max_length=30, null=True)
-    #phone = models.CharField(max_length=30, null=True)
+    phone = models.CharField(max_length=30, null=True)
 
