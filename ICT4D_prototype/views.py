9330f4770d7d4a77fc8bf09f7174412900a9f599
sfikouris <parissf15@hotmail.com>
2021-05-12 11:39:34 +0300

    asd

--- ICT4D_prototype/views.py
+++ ICT4D_prototype/views.py
@@ -49,41 +49,41 @@ def result(request):
 @csrf_exempt
 def data(request):
     form = DocumentForm(request.POST, request.FILES)
-    if form.is_valid():
+    #if form.is_valid():
         
-        instance = Document()
+    instance = Document()
         
-        rec_commune=request.FILES['rec_commune']
-        rec_location=request.FILES['rec_location']
-        caller = request.POST['existingcaller']
-        if caller == 0:
-            rec_name=request.FILES['rec_name']
+    rec_commune=request.FILES['rec_commune']
+    rec_location=request.FILES['rec_location']
+    caller = request.POST['existingcaller']
+    if caller == 0:
+        rec_name=request.FILES['rec_name']
 
 
-        tree_num = int(request.POST['tree_num'])
-        tree = trees[tree_num-1]
+    tree_num = int(request.POST['tree_num'])
+    tree = trees[tree_num-1]
 
-        cercle_num = int(request.POST['cercle_num'])
-        cercle = cercles[cercle_num-1]
+    cercle_num = int(request.POST['cercle_num'])
+    cercle = cercles[cercle_num-1]
 
-        tree_count = request.POST['tree_count']
-        chosen_language = request.POST['chosen_language']
-        phone_number = request.POST['phone']
+    tree_count = request.POST['tree_count']
+    chosen_language = request.POST['chosen_language']
+    phone_number = request.POST['phone']
 
-        instance.rec_commune = rec_commune
-        instance.rec_location = rec_location
-        if caller == 0:
-            instance.rec_name = rec_name
-        instance.cercle_num = cercle
-        instance.tree_num = tree
-        instance.tree_count = tree_count
-        instance.chosen_language = chosen_language
-        instance.phone = phone_number
-        instance.save()
-        return HttpResponse(status=200)
+    instance.rec_commune = rec_commune
+    instance.rec_location = rec_location
+    if caller == 0:
+        instance.rec_name = rec_name
+    instance.cercle_num = cercle
+    instance.tree_num = tree
+    instance.tree_count = tree_count
+    instance.chosen_language = chosen_language
+    instance.phone = phone_number
+    instance.save()
+    return HttpResponse(status=200)
         
    
-    return HttpResponse(status=400)
+    #return HttpResponse(status=400)
 
 
 def numberCheck(request):
