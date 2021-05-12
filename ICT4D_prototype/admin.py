5d5f35f99055a952c770ad08372138bf148830ba
sfikouris <parissf15@hotmail.com>
2021-05-12 16:01:10 +0300

    cleaning

--- ICT4D_prototype/admin.py
+++ ICT4D_prototype/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import treeaid
-from .models import Person
 from .models import Document
 
-admin.site.register(Person)
 admin.site.register(treeaid)
 admin.site.register(Document)
--- ICT4D_prototype/urls.py
+++ ICT4D_prototype/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='ICT4_prototype-home'),
     path('data', views.data, name='ICT4_prototype-data'),
-    path('dummy', views.dummy, name='ICT4_prototype-dummy'),
     path('simple_upload', views.simple_upload, name='ICT4_prototype-simple_upload'),
     path('numberCheck', views.numberCheck, name='ICT4_prototype-numberCheck'),
     path('result', views.result, name='result'),
