5a549e0dba4a34e2dcb9dd07f839b9cffd2ddb32
sfikouris <parissf15@hotmail.com>
2021-05-12 16:20:17 +0300

    navigation

--- ICT4D_prototype/urls.py
+++ ICT4D_prototype/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='ICT4_prototype-home'),
+    path('home', views.home, name='ICT4_prototype-home'),
     path('data', views.data, name='ICT4_prototype-data'),
     path('simple_upload', views.simple_upload, name='ICT4_prototype-simple_upload'),
     path('numberCheck', views.numberCheck, name='ICT4_prototype-numberCheck'),
