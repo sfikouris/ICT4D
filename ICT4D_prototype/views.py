3d6be2f6dd180d26f18de9bcb059575f3869fbe7
sfikouris <parissf15@hotmail.com>
2021-05-12 11:07:30 +0300

    f

--- ICT4D_prototype/views.py
+++ ICT4D_prototype/views.py
@@ -8,7 +8,6 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.core import serializers
-import datetime
 
 import folium
 
@@ -35,14 +34,11 @@ def result(request):
     phone_number = request.POST['phone_number']
     tree_count = request.POST['tree_num']
     
-    x = datetime.datetime.now()
 
     data = treeaid()
     data.cercle = cercle
     data.tree = tree
     data.tree_count = tree_count
-    data.time = x
-    data.phone_number = phone_number + str(x.hour) + str(x.minute)
     data.save()
 
     query_results = treeaid.objects.order_by('phone_number') 
@@ -77,10 +73,7 @@ def data(request):
         chosen_language = request.POST['chosen_language']
         phone_number = request.POST['phone']
         
-        x = datetime.datetime.now()
         
-        rec_commune.name = str(phone_number) + str(x.hour) + str(x.minute) 
-        src_commune = "media/commune/" + rec_commune.name + ".wav"
         instance = Document()
         
         instance.rec_commune = rec_commune
@@ -92,7 +85,6 @@ def data(request):
         instance.tree_count = tree_count
         instance.chosen_language = chosen_language
         instance.phone = phone_number
-        instance.src_commune = src_commune
         instance.save()
         return HttpResponse(status=200)
         
